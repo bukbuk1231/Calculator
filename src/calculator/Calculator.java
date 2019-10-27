01d8ae3b2262366120beee1bd6be376a635b33d5
bukbuk1231 <loudakkk@hotmail.com>
2019-10-26 18:49:01 -0700

    Test for inputNumber() API

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,6 +1,22 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
+    private int n1, n2, op;
+    private Scanner scanner;
 
+    private static final char[] ops = {'+', '-', '*', '/', '%'};
+
+    public Calculator() {
+        scanner = new Scanner(System.in);
+        n1 = n2 = 0;
+    }
+
+    public double inputNumber(String input) {
+        System.out.print("Input your number: ");
+        return Double.valueOf(input);
+        // return scanner.nextDouble();
+    }
 }
--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -1,11 +1,31 @@
+import calculator.Calculator;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
 
 public class CalculatorTest {
 
+    Calculator calculator;
+
+    public CalculatorTest() {
+        calculator = new Calculator();
+    }
+
     @Test
-    public void inputNumberTest() {
-        fail();
+    public void inputTestValidDoubleWithoutDecimal() {
+        double n = calculator.inputNumber("1");
+        assertEquals(n, 1.0);
+    }
+
+    @Test
+    public void inputTestValidDoubleWithDecimal() {
+        double n = calculator.inputNumber("1.823");
+        assertEquals(n, 1.823);
+    }
+
+    @Test (expected = NumberFormatException.class)
+    public void inputTestInvalidString() {
+        double n = calculator.inputNumber("abvc");
     }
 }
